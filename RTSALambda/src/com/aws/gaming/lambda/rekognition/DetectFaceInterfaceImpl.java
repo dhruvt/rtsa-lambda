95ba55be368ca897363142509f1f45e8f6318c0a
Thukral <dhruv@amazon.com>
2017-09-25 15:19:30 -0700

    Updated AWS Credentials Provider

--- RTSALambda/src/com/aws/gaming/lambda/rekognition/DetectFaceInterfaceImpl.java
+++ RTSALambda/src/com/aws/gaming/lambda/rekognition/DetectFaceInterfaceImpl.java
@@ -9,6 +9,7 @@ import java.util.List;
 import com.amazonaws.AmazonClientException;
 import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.EnvironmentVariableCredentialsProvider;
 import com.amazonaws.auth.profile.ProfileCredentialsProvider;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.rekognition.AmazonRekognition;
@@ -44,9 +45,13 @@ public class DetectFaceInterfaceImpl implements DetectFaceInterface {
 			ProfileCredentialsProvider pcp = new ProfileCredentialsProvider("default");
 			credentials = pcp.getCredentials();			
 		}catch (Exception e){
-			throw new AmazonClientException("Cannot load the credentials from the credential profiles file. "
+			System.out.println("Cannot load the credentials from the credential profiles file. "
 		            + "Please make sure that your credentials file is at the correct "
-		            + "location (/Users/userid.aws/credentials), and is in a valid format.", e);
+		            + "location (/Users/userid.aws/credentials), and is in a valid format." + e);
+		} finally{
+			System.out.println("Loading Environment Credentials");
+			EnvironmentVariableCredentialsProvider ecp = new EnvironmentVariableCredentialsProvider();
+			credentials = ecp.getCredentials();
 		}
 	}
 
